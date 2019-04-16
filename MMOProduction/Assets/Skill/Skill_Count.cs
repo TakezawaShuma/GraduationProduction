e04d69ced39966a934a4e48117ac49ccf5fc7490
taikihayakawa <kunieda0419@gmail.com>
2019-04-16 12:28:49 +0900

    UIの設定

---
+++ MMOProduction/Assets/Skill/Skill_Count.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Skill_Count : MonoBehaviour
+{
+    int score = 0;
+    GameObject score_bord;
+
+    //UPボタンに登録
+    public void Up_Button()
+    {
+        score = score + 1;
+        Debug.Log("あがった");
+    }
+
+    //DOWNボタンに登録
+    public void Down_Button()
+    {
+        score = score - 1;
+        Debug.Log("下がった");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MMOProduction/Assets/Skill/Skill_Number.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Skill_Number : MonoBehaviour
+{
+    //Textのオブジェクト
+    public GameObject score_object = null;
+
+
+    public Text score_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // オブジェクトからTextコンポーネントを取得
+       score_text = score_object.GetComponent<Text>();
+        // テキストの表示を入れ替える
+        score_text.text = "00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //スキルカウントスクリプトに呼び出す関数
+    public void Set_Score(int score)
+    {
+        score_text.text = score.ToString();
+    }
+}
