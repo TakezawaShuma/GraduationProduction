75f42aa79cff617ae12f3bd8c35ddb378ccd8f34
SugiuraMakoto <margin9876@gmail.com>
2019-04-18 17:25:49 +0900

    プレイヤーの動きを更新

---
+++ MMOProduction/Assets/MSScript/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public float X;
+    public float Y;
+    public float Z;
+
+    public float HP;
+    public float MP;
+
+    public float Direction;
+}
