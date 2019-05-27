e3df70b640a72e9f9f1eeeac4b30a137c258976c
SugiuraMakoto <margin9876@gmail.com>
2019-05-27 13:35:10 +0900

    ステートパターンを追加

---
+++ MMOProduction/Assets/Script/State/AutoRunState.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRunState : BaseState
+{
+    // 速度
+    private Vector3 velocity;
+
+    private static BaseState instance;
+
+    public static BaseState Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new AutoRunState();
+            }
+
+            return instance;
+        }
+    }
+
+    public override void Execute()
+    {
+        // 前に進む
+        velocity = new Vector3(-1, 0, 0) * playerSetting.DS * Time.deltaTime;
+
+        playerController.Move(velocity);
+
+        if (Input.GetKeyDown(playerSetting.FKey) || Input.GetKeyDown(playerSetting.BKey) || Input.GetKeyDown(playerSetting.LKey) || Input.GetKeyDown(playerSetting.RKey))
+        {
+            playerController.ChangeState(KeyMoveState.Instance);
+        }
+
+        Debug.Log("Auto");
+    }
+}
