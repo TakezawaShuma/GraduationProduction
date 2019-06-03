5fbc65206e6ca0a107cfb68aa1b51e6d147b8e0d
Naoki Hayashi <pastelpalettes.yp@gmail.com>
2019-06-03 13:48:59 +0900

    インベントリの完成

---
+++ MMOProduction/Assets/Inventory/Script/SwipeMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SwipeMove : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    // ドラックが開始したとき呼ばれる.
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Debug.Log(1);
+        if (eventData.button != PointerEventData.InputButton.Left) return;
+    }
+
+    // ドラック中に呼ばれる.
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left) return;
+        this.GetComponent<RectTransform>().anchoredPosition += eventData.delta;
+    }
+
+    // ドラックが終了したとき呼ばれる.
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
+
+}
