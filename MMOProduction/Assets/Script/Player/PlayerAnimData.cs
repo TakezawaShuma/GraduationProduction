2ae5948d943e957b0d1cb3a9a5f53f2a9bf33943
s-tama <syuuki0105@gmail.com>
2019-09-26 14:48:11 +0900

    プレイヤーをユキちゃんに差し替えと その他キャラの追加

---
+++ MMOProduction/Assets/Script/Player/PlayerAnimData.cs
@@ -0,0 +1,31 @@
+﻿//
+// PlayerAnimData.cs
+//
+
+using UnityEngine;
+
+// プレイヤーアニメーションデータ
+public class PlayerAnimData
+{
+
+    private Animator _animator;
+    private GameObject _owner;
+
+
+    public PlayerAnimData(GameObject owner)
+    {
+        _animator = owner.GetComponent<Animator>();
+    }
+
+    // アニメーションデータ1の時に使用
+    public void Move(float speed)
+    {
+        _animator.SetFloat("speed", speed);
+    }
+
+    // アニメーションデータ2の時に使用
+    public void Move(bool state)
+    {
+        _animator.SetBool("walk", state);
+    }
+}
