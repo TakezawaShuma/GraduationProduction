9b820ce047e743add54fe34822f7420f43fb16f7
HoriuchiYousuke2020 <horiuchi199720@gmail.com>
2019-10-11 09:54:33 +0900

    no message

---
+++ MMOProduction/Assets/Script/UI/UIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    GameObject[] prefabs;
+
+    enum Window
+    {
+        CHARACTOR,
+        ITEM,
+        SKILL,
+        QEST,
+        PARTY,
+        CHAT,
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void  PushKeyEvent(GameObject obj)
+    {
+        if(obj.activeInHierarchy != true)
+        {
+            obj.SetActive(true);
+        }
+        else
+        {
+            obj.SetActive(false);
+        }
+    }
+}
