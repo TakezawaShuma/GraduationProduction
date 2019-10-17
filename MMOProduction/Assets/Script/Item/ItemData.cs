be1a1246b4abc258b0017954af2fc000ced70e3d
SugiuraMakoto <margin9876@gmail.com>
2019-10-17 11:49:07 +0900

    アイテムデータの構造体追加

---
+++ MMOProduction/Assets/Script/Item/ItemData.cs
@@ -0,0 +1,39 @@
+﻿/*/////////////////////////////////////////////////////////
+||
+||	ファイル名：ItemData.cs
+||
+||	概要：アイテムデータの構造体
+||
+||	作成者：杉浦諒
+||
+||	作成日：2019/10/17
+||
+||	更新日：2019/10/17
+||
+*//////////////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct ItemData
+{
+    private int     id;         // アイテムのID
+    private int     iconId;     // アイコンのID
+    private string  name;       // アイテムの名前
+    private string  detail;       // 説明文
+    private int     maxNum;     // 最大所持数
+    private int     currentNum; // 現在所持数
+    private int     price;      // 値段
+
+    public ItemData(int id = 0, int iconId = 0, string name = "NoName", string detail = "NoText", int maxNum = 0, int currentNum = 0, int price = 0)
+    {
+        this.id         = id;
+        this.iconId     = iconId;
+        this.name       = name;
+        this.detail     = detail;
+        this.maxNum     = maxNum;
+        this.currentNum = currentNum;
+        this.price      = price;
+    }
+}
--- MMOProduction/Assets/Script/Status/Status.cs
+++ MMOProduction/Assets/Script/Status/Status.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿/*/////////////////////////////////////////////////////////
+||
+||	ファイル名：Status.cs
+||
+||	概要：ステータスの構造体
+||
+||	作成者：杉浦諒
+||
+||	作成日：2019/10/17
+||
+||	更新日：2019/10/17
+||
+*//////////////////////////////////////////////////////////
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
 public struct Status
 {
-    public int hitPoint;
-    public int magicPoint;
-    public int strength;
-    public int vitality;
-    public int intelligence;
-    public int mind;
-    public int dexterity;
-    public int agility;
+    public int hitPoint;    // 体力ポイント
+    public int magicPoint;  // 魔法ポイント
+    public int strength;    // 筋力
+    public int vitality;    // 持続力
+    public int intelligence;// 知性
+    public int mind;        // 精神力
+    public int dexterity;   // 器用さ
+    public int agility;     // 俊敏さ
 
     public Status(int hp = 0, int mp = 0, int str = 0, int vit = 0, int inte = 0, int min = 0, int dex = 0, int agi = 0)
     {
