9b607c7c05a33f70bf83e057c4a7fa1873767f2c
SugiuraMakoto <margin9876@gmail.com>
2019-10-18 09:20:55 +0900

    装備と消耗品も追加

--- MMOProduction/Assets/Script/Item/ItemData.cs
+++ MMOProduction/Assets/Script/Item/ItemData.cs
@@ -2,7 +2,7 @@
 ||
 ||	ファイル名：ItemData.cs
 ||
-||	概要：アイテムデータの構造体
+||	概要：アイテムデータのクラス
 ||
 ||	作成者：杉浦諒
 ||
@@ -16,20 +16,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public struct ItemData
+public class ItemData
 {
-    private int     id;         // アイテムのID
-    private int     iconId;     // アイコンのID
-    private string  name;       // アイテムの名前
-    private string  detail;       // 説明文
-    private int     maxNum;     // 最大所持数
-    private int     currentNum; // 現在所持数
-    private int     price;      // 値段
+    public int     id;         // アイテムのID
+    public int     iconId;     // アイコンのID
+    public int     effectId;   // 使用するエフェクトのID
+    public string  name;       // アイテムの名前
+    public string  detail;       // 説明文
+    public int     maxNum;     // 最大所持数
+    public int     currentNum; // 現在所持数
+    public int     price;      // 値段
 
-    public ItemData(int id = 0, int iconId = 0, string name = "NoName", string detail = "NoText", int maxNum = 0, int currentNum = 0, int price = 0)
+    public ItemData(int id = 0, int iconId = 0, int effectId = 0, string name = "NoName", string detail = "NoText", int maxNum = 0, int currentNum = 0, int price = 0)
     {
         this.id         = id;
         this.iconId     = iconId;
+        this.effectId   = effectId;
         this.name       = name;
         this.detail     = detail;
         this.maxNum     = maxNum;
