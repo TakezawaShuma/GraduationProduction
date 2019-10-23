cc0da326527ee612fb90ce3f935b0376a61a6cf1
HoriuchiYousuke2020 <horiuchi199720@gmail.com>
2019-10-23 11:57:50 +0900

    no message

--- MMOProduction/Assets/Editor/skill_Editor.cs
+++ MMOProduction/Assets/Editor/skill_Editor.cs
@@ -15,12 +15,14 @@ public class skill_Editor : EditorWindow
     /// window作成
     /// </summary>
     [MenuItem("GameSetting/Skill")]
-    private static void Open(){
+    private static void Open()
+    {
         tablelist = Resources.Load<skill_table>("GameData\\skill_data");
         // 生成
         window = GetWindow<skill_Editor>("skill_setting");
-        window.maxSize = window.minSize = new Vector2 (440, 500);
-        foreach(var table in tablelist.tables) {
+        window.maxSize = window.minSize = new Vector2(450, 500);
+        foreach (var table in tablelist.tables)
+        {
             delete_flag.Add(false);
         }
     }
@@ -37,91 +39,131 @@ public class skill_Editor : EditorWindow
             EditorGUILayout.LabelField("----------------------------------number" + (i + 1).ToString() + "----------------------------------");
 
             EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField("ID : ", GUILayout.Width(20));
+            EditorGUILayout.LabelField("ID", GUILayout.Width(20));
             table.id = EditorGUILayout.IntField(table.id, GUILayout.Width(40));
-            EditorGUILayout.LabelField("最大Lv : ", GUILayout.Width(40));
-            table.max_point = EditorGUILayout.IntField(table.max_point, GUILayout.Width(40));
-            EditorGUILayout.LabelField("スキル種類 : ", GUILayout.Width(60));
-            table.skillType = (skill_type)EditorGUILayout.EnumPopup(table.skillType, GUILayout.Width(100));
+            EditorGUILayout.LabelField("名前", GUILayout.Width(35));
+            table.name = EditorGUILayout.TextArea(table.name, GUILayout.Width(100));
+            EditorGUILayout.LabelField("", GUILayout.Width(50));
+            EditorGUILayout.LabelField("エフェクトID", GUILayout.Width(80));
+            table.effectId = EditorGUILayout.IntField(table.effectId, GUILayout.Width(40));
 
-            if (!delete_flag[i]) {
-                if(GUILayout.Button("削除",GUILayout.Width(80))) {
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUILayout.LabelField("最大Lv", GUILayout.Width(50));
+            table.maxLv = EditorGUILayout.IntField(table.maxLv, GUILayout.Width(40));
+
+            EditorGUILayout.LabelField("親ID", GUILayout.Width(40));
+            table.pearentID = EditorGUILayout.IntField(table.pearentID, GUILayout.Width(40));
+
+            EditorGUILayout.LabelField("取得必要ポイント", GUILayout.Width(100));
+            table.pearentPoint = EditorGUILayout.IntField(table.pearentPoint, GUILayout.Width(40));
+
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUILayout.LabelField("効果時間", GUILayout.Width(50));
+            table.time = EditorGUILayout.IntField(table.time, GUILayout.Width(40));
+
+            EditorGUILayout.LabelField("ヘイト値", GUILayout.Width(50));
+            table.hate = EditorGUILayout.IntField(table.hate, GUILayout.Width(40));
+
+            EditorGUILayout.LabelField("武器", GUILayout.Width(50));
+            table.weapon = EditorGUILayout.IntField(table.weapon, GUILayout.Width(40));
+
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUILayout.LabelField("ターゲット種類 : ", GUILayout.Width(60));
+            table.targetType = (target_type)EditorGUILayout.EnumPopup(table.targetType, GUILayout.Width(100));
+
+            EditorGUILayout.LabelField("範囲種類 : ", GUILayout.Width(60));
+            table.rangeType = (range_type)EditorGUILayout.EnumPopup(table.rangeType, GUILayout.Width(100));
+
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+
+
+            EditorGUILayout.LabelField("影響ステータス", GUILayout.Width(80));
+
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+
+            string[] nameTable = { "HP", "MP", "STR", "VIT", "INT", "MND", "DEX", "AGI", "状態異常", "威力" };
+            int[] statusTable = { 0,0,0,0,0,0,0,0,0,0 };
+            for (int j = 0; j < 5; j++)
+            {
+                EditorGUILayout.LabelField(nameTable[j].ToString(), GUILayout.Width(50));
+                statusTable[j] = EditorGUILayout.IntField(statusTable[j], GUILayout.Width(30));
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
+
+            for (int j = 5; j < 10; j++)
+            {
+                EditorGUILayout.LabelField(nameTable[j].ToString(), GUILayout.Width(50));
+                statusTable[j] = EditorGUILayout.IntField(statusTable[j], GUILayout.Width(30));
+            }
+
+            table.hp = statusTable[0];
+            table.mp = statusTable[1];
+            table.str = statusTable[2];
+            table.vit = statusTable[3];
+            table.inte = statusTable[4];
+            table.mnd = statusTable[5];
+            table.dex = statusTable[6];
+            table.agi = statusTable[7];
+            table.effect = statusTable[8];
+            table.condition = statusTable[9];
+
+
+
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.LabelField("説明文");
+            table.detail = EditorGUILayout.TextArea(table.detail, GUILayout.Width(430));
+
+            EditorGUILayout.BeginHorizontal();
+
+            if (!delete_flag[i])
+            {
+                if (GUILayout.Button("削除", GUILayout.Width(80)))
+                {
                     delete_flag[i] = true;
                 }
             }
-            else {
-                if (GUILayout.Button("OK",GUILayout.Width(40))) {
+            else
+            {
+                if (GUILayout.Button("OK", GUILayout.Width(40)))
+                {
                     tablelist.tables.Remove(table);
                     delete_flag.Remove(delete_flag[i]);
                     break;
                 }
                 if (GUILayout.Button("NO", GUILayout.Width(40))) delete_flag[i] = false;
             }
-
             EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.BeginHorizontal();
-
-            switch (table.skillType)
-            {
-                case skill_type.ATTACK:
-                    EditorGUILayout.LabelField("威力 : ", GUILayout.Width(40));
-                    table.effect = EditorGUILayout.IntField(table.effect, GUILayout.Width(80));
-                    break;
-
-
-                case skill_type.BUFF:
-                    EditorGUILayout.LabelField("バフの種類", GUILayout.Width(80));
-                    table.buffType = (buff_type)EditorGUILayout.EnumPopup(table.buffType, GUILayout.Width(100));
-                    switch (table.buffType)
-                    {
-                        case buff_type.ATTACK:
-                            EditorGUILayout.LabelField("上昇量 : ", GUILayout.Width(40));
-                            table.effect = EditorGUILayout.IntField(table.effect, GUILayout.Width(80));
-                            break;
-                        case buff_type.DEFFENSE:
-                            EditorGUILayout.LabelField("上昇量 : ", GUILayout.Width(40));
-                            table.effect = EditorGUILayout.IntField(table.effect, GUILayout.Width(80));
-                            break;
-                        case buff_type.HEEL:
-                            EditorGUILayout.LabelField("回復量 : ", GUILayout.Width(40));
-                            table.effect = EditorGUILayout.IntField(table.effect, GUILayout.Width(80));
-                            break;
-                        case buff_type.NOME:
-                            EditorGUILayout.LabelField("何を設定すれば・・・・？", GUILayout.Width(160));
-                            break;
-                        default: break;
-                    }
-                    break;
-
-                case skill_type.NUME:
-                    EditorGUILayout.LabelField("何を設定すれば・・・・？", GUILayout.Width(160));
-                    break;
-
-                default: break;
-            }
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.LabelField("説明文");
-            table.description = EditorGUILayout.TextArea(table.description);
             i++;
         }
         EditorGUILayout.EndScrollView();
 
         EditorGUILayout.BeginHorizontal();
+
         if (GUILayout.Button("追加"))
         {
             tablelist.tables.Add(new skill_table.skill_data());
             delete_flag.Add(false);
         }
+
         if (GUILayout.Button("最後尾の削除"))
         {
             tablelist.tables.Remove(tablelist.tables[tablelist.tables.Count - 1]);
             delete_flag.Remove(delete_flag[delete_flag.Count - 1]);
         }
- 
+
         EditorGUILayout.EndHorizontal();
-        
+
         // ボタンの配置
         if (GUILayout.Button("保存"))
             textSave(JsonUtility.ToJson(tablelist));
