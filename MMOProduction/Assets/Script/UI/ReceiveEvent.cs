e08e46b06ef2bda8a1abcf1d59893c88c621f01e
HoriuchiYousuke2020 <horiuchi199720@gmail.com>
2019-10-23 11:29:24 +0900

    インベントリ追加

---
+++ MMOProduction/Assets/Script/UI/ReceiveEvent.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReceiveEvent : MonoBehaviour
+{
+    private Vector3 startPosition;
+    GameObject hitObject;
+
+    private Vector3 length;
+    // 押した時
+    public void MyPointerDownUI()
+    {
+
+        length = this.transform.position - Input.mousePosition;
+        startPosition = this.transform.position;
+        ChangeAlpha(0.4f);
+    }
+
+    // 離した時
+    public void MyPointerUpUI()
+    {
+        ChangeAlpha(1.0f);
+        if (hitObject != null)
+        {
+            //hitObject.GetComponent<Image>().sprite = this.GetComponent<Image>().sprite;
+            hitObject.GetComponent<Image>().color = this.GetComponent<Image>().color;
+        }
+        this.transform.position = startPosition;
+    }
+
+    // ドラッグ時の関数
+    public void MyDragUI()
+    {
+        transform.position = Input.mousePosition + length;
+    }
+
+
+    // Imageのalpha値の変更
+    public void ChangeAlpha(float alpha)
+    {
+        var component = GetComponent<Image>();
+        var color = component.color;
+        color.a = alpha;
+        component.color = color;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Slot")
+        {
+            Debug.Log("UI衝突");
+            Debug.Log(length);
+            hitObject = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Slot")
+        {
+            Debug.Log("UI衝突");
+            hitObject = null;
+        }
+    }
+}
---
+++ MMOProduction/Assets/Script/UI/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] ui;
+
+    [SerializeField]
+    private KeyCode[] key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ui != null)
+        {
+            for (int i = 0; i < ui.Length; i++)
+            {
+                if(Input.GetKeyDown(key[i]))
+                {
+                    if(ui[i].activeInHierarchy)
+                    {
+                        ui[i].SetActive(false);
+                    }
+                    else
+                    {
+                        ui[i].SetActive(true);
+                    }
+                }
+            }
+        }
+    }
+}
