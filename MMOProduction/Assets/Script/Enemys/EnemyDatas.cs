35053d2b0a4c3d037436f1ab3e4d1d70643c6f64
Naoki Hayashi <pastelpalettes.yp@gmail.com>
2019-11-06 11:15:53 +0900

    コメントの追加とコメント化していた行の削除

--- MMOProduction/Assets/Script/Enemys/EnemyDatas.cs
+++ MMOProduction/Assets/Script/Enemys/EnemyDatas.cs
@@ -5,26 +5,20 @@ using UnityEngine;
 
 public class EnemyManager
 {
+    //EnemyData List
     List<EnemyData> el = new List<EnemyData>();
+
+    //Enemyの登録 id =ID modelName = Prefab名
     public void AddEnemy(int id,string modelName)
     {
-        
         EnemyData ed = new EnemyData();
         ed.id_ = id;
         Debug.Log("AddEnemy ID:" + id);
-        //switch (id)
-        //{
-        //    case 0:
-        //        ed.prefab_ = (GameObject)Resources.Load("Prefabs/Forest Bunny Black");
-        //        break;
-        //    default:
-        //        ed.prefab_ = (GameObject)Resources.Load("Prefabs/Forest Bunny Black");
-        //        break;
-        //}
         ed.prefab_ = (GameObject)Resources.Load("Prefabs/"+modelName);
         el.Add(ed);
     }
 
+    //指定されたIDのEnemyのPrefabを返す
     public GameObject GetEnemyPrefab(int id)
     {
         foreach(var v in el)
@@ -37,21 +31,12 @@ public class EnemyManager
         Debug.Log("存在しないID");
         return null;
     }
-
-
-
-//    public GameObject EnemyModel(int id_)
-//    {
-//        
-//    }
-
-    //int master;
-    //int model;
-    //int  maxHp;
 }
 
 public class EnemyData
 {
+    //Enemy ID
     public int id_;
+    //Enemy Prefab
     public GameObject prefab_;
 }
