3079e56d026816250b4316cf9c1fc0a8017076f9
HoriuchiYousuke2020 <horiuchi199720@gmail.com>
2019-11-12 09:19:22 +0900

    更新

---
+++ MMOProduction/Assets/Script/Setting/Build.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Build {
+    [RuntimeInitializeOnLoadMethod]
+    static void OnRuntimeMethodLoad() {
+        Screen.SetResolution(1920, 1080, true);
+    }
+}
+
