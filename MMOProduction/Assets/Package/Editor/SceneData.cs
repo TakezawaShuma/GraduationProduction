d227316ce7f6581142fa8318f4f12a77d69f357b
s-tama <syuuki0105@gmail.com>
2019-11-29 11:03:22 +0900

    剣の軌跡エフェクトを実装

---
+++ MMOProduction/Assets/Package/Editor/SceneData.cs
@@ -0,0 +1,236 @@
+﻿///
+/// ReferenceExplorer
+/// 
+/// Copyright (c) 2014 Tatsuhiko Yamamura
+/// Released under the MIT license
+// / http://opensource.org/licenses/mit-license.php
+/// 
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace ReferenceExplorer
+{
+    /// <summary>
+    /// シーン全体の情報を格納する
+    /// </summary>
+    public class SceneData
+    {
+        /// <summary>
+        /// Initializes the <see cref="ReferenceExplorer.SceneData"/> class.
+        /// </summary>
+        static SceneData()
+        {
+            EditorApplication.update += () => {
+                frameCount++;
+            };
+            IsSelected = false;
+            IsRegex = false;
+        }
+
+        /// <summary>
+        /// 検索範囲を選択中のオブジェクトに限定する
+        /// </summary>
+        public static bool IsSelected { get; set; }
+
+        /// <summary>
+        /// 検索に正規表現を使用する
+        /// </summary>
+        public static bool IsRegex { get; set; }
+
+        /// <summary>
+        /// 検索に使用する全てのMonobehaviour
+        /// </summary>
+        static List<MonoBehaviour> allMonobehaviourList = new List<MonoBehaviour>();
+
+        /// <summary>
+        /// シーン内の全オブジェクト
+        /// </summary>
+        static List<GameObject> allGameObjectList = new List<GameObject>();
+
+        /// <summary>
+        /// エディタのフレームカウント
+        /// </summary>
+        public static long frameCount { get; private set; }
+
+        /// <summary>
+        /// 更新フレーム。frameCountと一致しなければ更新しない等の用途
+        /// </summary>
+        static long monobehaviourLastUpdateFrame = 0, gameobjectLastUpdateFrame = 0;
+
+        /// <summary>
+        /// シーンにある全てのGameObject。isSelectedにチェックがある場合は選択中のオブジェクトを取得する
+        /// </summary>
+        /// <value>GameObject一覧</value>
+        public static List<GameObject> SelectedObjects
+        {
+            get
+            {
+                if (IsSelected)
+                {
+                    return new List<GameObject>(Selection.gameObjects);
+                }
+                else
+                {
+                    return AllObjects;
+                }
+            }
+        }
+
+        /// <summary>
+        /// シーン内の全コンポーネントを取得する。isSelectedにチェックがある場合は選択中のオブジェクトのコンポーネントを取得する
+        /// </summary>
+        /// <value>Monobehaviour一覧</value>
+        public static List<MonoBehaviour> SelectedMonobehaviours
+        {
+            get
+            {
+                var monobehaviourList = new List<MonoBehaviour>();
+
+                if (IsSelected)
+                {
+                    foreach (var obj in Selection.gameObjects)
+                    {
+                        monobehaviourList.AddRange(AllComponents.FindAll(item => item.gameObject == obj));
+                    }
+                    return monobehaviourList;
+                }
+                else
+                {
+                    return AllComponents;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets all objects.
+        /// </summary>
+        /// <value>All objects.</value>
+        public static List<GameObject> AllObjects
+        {
+            get
+            {
+                if (frameCount != gameobjectLastUpdateFrame)
+                {
+                    UpdateAllObjectList();
+                    gameobjectLastUpdateFrame = frameCount;
+                }
+
+                return new List<GameObject>(allGameObjectList);
+            }
+        }
+
+        /// <summary>
+        /// Gets all component.
+        /// </summary>
+        /// <value>All component.</value>
+        public static List<MonoBehaviour> AllComponents
+        {
+            get
+            {
+                if (monobehaviourLastUpdateFrame != frameCount)
+                {
+                    UpdateAllMonobehaviourList();
+                    monobehaviourLastUpdateFrame = frameCount;
+                }
+                return new List<MonoBehaviour>(allMonobehaviourList);
+            }
+        }
+
+        /// <summary>
+        ///Component一覧を更新する
+        /// </summary>
+        static void UpdateAllMonobehaviourList()
+        {
+            allMonobehaviourList.Clear();
+            allMonobehaviourList.AddRange(GetComponentsInList<MonoBehaviour>(AllObjects));
+        }
+
+        /// <summary>
+        /// GameObject一覧（deactive含む）を更新する
+        /// </summary>
+        static void UpdateAllObjectList()
+        {
+            allGameObjectList.Clear();
+            foreach (GameObject obj in (GameObject[])Resources.FindObjectsOfTypeAll(typeof(GameObject)))
+            {
+
+                if (obj.hideFlags == HideFlags.NotEditable || obj.hideFlags == HideFlags.HideAndDontSave)
+                    continue;
+
+                if (Application.isEditor)
+                {
+                    string sAssetPath = AssetDatabase.GetAssetPath(obj.transform.root.gameObject);
+                    if (!string.IsNullOrEmpty(sAssetPath))
+                        continue;
+                }
+
+                allGameObjectList.Add(obj);
+            }
+        }
+
+        public static IEnumerable<T> GetComponentsInList<T>(IEnumerable<GameObject> gameObjects) where T : Component
+        {
+            var componentList = new List<T>();
+
+            foreach (var obj in gameObjects)
+            {
+                var component = obj.GetComponent<T>();
+                if (component != null)
+                {
+                    componentList.Add(component);
+                }
+            }
+            return componentList;
+        }
+
+        /// <summary>
+        /// searchの内容がInputに含まれることを確認する。isRegexにチェックを入れると正規表現で検索する
+        /// </summary>
+        /// <param name="input">インプット</param>
+        /// <param name="search">検索キーワード</param>
+        public static bool Match(string input, string search)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            if (IsRegex)
+            {
+                var match = Regex.Match(input, search);
+                return match.Success;
+            }
+            else
+            {
+                var result = input.IndexOf(search);
+                return (result != 0 && result != -1);
+            }
+        }
+
+        /// <summary>
+        /// ユニークなMonobehaviourを継承したコンポーネント一覧を取得する
+        /// </summary>
+        /// <returns>検索するオブジェクト一覧</returns>
+        /// <param name="objects">Monobehaviour一覧</param>
+        public static List<MonoBehaviour> GetUniqueMonobehaviour(IEnumerable<GameObject> objects)
+        {
+            var uniqueMonobehaviourList = new List<MonoBehaviour>();
+            foreach (var obj in objects)
+            {
+                foreach (var monobehaviour in obj.GetComponents<MonoBehaviour>())
+                {
+                    if (!uniqueMonobehaviourList.Exists(item => item.GetType() == monobehaviour.GetType()))
+                    {
+                        uniqueMonobehaviourList.Add(monobehaviour);
+                    }
+                }
+            }
+
+            return uniqueMonobehaviourList;
+        }
+    }
+}
---
+++ MMOProduction/Assets/Resources/Models/Effects/SwordTrail/Scripts/SwordTrailCreator.cs
@@ -0,0 +1,148 @@
+﻿//
+// SwordTrailCreator.cs
+//
+// 剣の軌跡を描くメッシュを作成するクラス
+//
+
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class SwordTrailCreator : MonoBehaviour
+{
+    /// <summary>
+    /// 頂点データ
+    /// </summary>
+    public struct Vertex
+    {
+        public Vector3 pos;
+        public Vector2 uv;
+
+        public Vertex(Vector3 pos, Vector2 uv)
+        {
+            this.pos = pos;
+            this.uv = uv;
+        }
+    }
+
+
+    // 軌跡用四角形メッシュの表示個数
+    private static readonly int CREATE_NUM = 10;
+
+
+    // 軌跡描画用の変換データ
+    [SerializeField]
+    private Transform _startPoint = null;   // 剣元
+    [SerializeField]
+    private Transform _endPoint = null;     // 剣先
+    [SerializeField]
+    private Transform _parent = null;       // このエフェクトの親オブジェクト
+
+    // 頂点データリスト
+    private List<Vertex> _vertices;
+    // 頂点インデックスリスト
+    private List<int> _indices;
+
+    // 剣先の座標リスト
+    private List<Vector3> _startPoints;     // 剣元
+    private List<Vector3> _endPoints;       // 剣先
+
+    // メッシュ描画用
+    private Mesh _mesh;
+
+
+    private void Start()
+    {
+        // 各リストの作成
+        _vertices = new List<Vertex>();
+        _indices = new List<int>();
+        _startPoints = new List<Vector3>();
+        _endPoints = new List<Vector3>();
+
+        // 各種コンポーネント
+        _mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    private void LateUpdate()
+    {
+        // 座標のリセット（メッシュの生成位置をずらさないため）
+        //this.transform.localScale = new Vector3(
+        //    1 / _parent.localScale.x,
+        //    1 / _parent.localScale.y,
+        //    1 / _parent.localScale.z
+        //    );
+        this.transform.rotation = Quaternion.Euler(0, 0, 0);
+        //this.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        this.transform.position = new Vector3(0, 0, 0);
+        //this.transform.localPosition = new Vector3(0, 0, 0);
+
+        // 必要頂点数を超えたら削除
+        if (_startPoints.Count >= CREATE_NUM + 1)
+        {
+            _startPoints.RemoveAt(0);
+            _endPoints.RemoveAt(0);
+        }
+
+        // 現在の剣の位置を登録
+        _startPoints.Add(_startPoint.position);
+        _endPoints.Add(_endPoint.position);
+
+        // 頂点がメッシュ数+1になったら斬撃用メッシュを作成
+        if (_startPoints.Count >= CREATE_NUM + 1)
+        {
+            CreateSwordTrailMesh();
+        }
+
+        //Debug.Log("頂点データ数 : " + _vertices.Count);
+    }
+
+    /// <summary>
+    /// 斬撃エフェクト用メッシュを作成する
+    /// </summary>
+    private void CreateSwordTrailMesh()
+    {
+        // メッシュのクリア
+        _mesh.Clear();
+
+        // リストのクリア
+        _vertices.Clear();
+        _indices.Clear();
+
+        float uvParam = 0f;
+        for (int i = 0; i < CREATE_NUM; i++)
+        {
+            // 頂点データ配列を作成し、リストに登録
+            Vertex[] newVertices = new Vertex[]
+            {
+                new Vertex(_startPoints[i], new Vector2(uvParam, 0f)),
+                new Vertex(_endPoints[i], new Vector2(uvParam, 1f)),
+                new Vertex(_startPoints[i + 1], new Vector2(uvParam + 1f / CREATE_NUM, 0f)),
+                new Vertex(_startPoints[i + 1], new Vector2(uvParam + 1f / CREATE_NUM, 0f)),
+                new Vertex(_endPoints[i], new Vector2(uvParam, 1f)),
+                new Vertex(_endPoints[i + 1], new Vector2(uvParam + 1f / CREATE_NUM, 1f)),
+            };
+            _vertices.AddRange(newVertices);
+
+            // 表示する四角形数で1を割って割合を加算
+            uvParam += 1f / CREATE_NUM;
+        }
+
+        // メッシュ用の三角形を登録した頂点で設定
+        Vector3[] pos = new Vector3[_vertices.Count];
+        Vector2[] uv = new Vector2[_vertices.Count];
+        for (int i = 0; i < _vertices.Count; i++)
+        {
+            pos[i] = _vertices[i].pos;
+            uv[i] = _vertices[i].uv;
+            _indices.Add(i);
+        }
+
+        _mesh.vertices = pos;
+        _mesh.uv = uv;
+        _mesh.triangles = _indices.ToArray();
+
+        // 再計算
+        _mesh.RecalculateBounds();
+        _mesh.RecalculateNormals();
+    }
+}
