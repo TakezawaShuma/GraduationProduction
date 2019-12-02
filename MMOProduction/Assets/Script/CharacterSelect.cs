4d2a559bb9be3cdcce4ae82824ce39ef3cd4bc0c
taikihayakawa <kunieda0419@gmail.com>
2019-12-02 17:49:31 +0900

    アニメーションの追加

---
+++ MMOProduction/Assets/Script/CharacterSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray,out hit, 50.0f))
+        {
+            if (hit.transform.gameObject.tag == "Player")
+            Debug.Log(hit.transform.name);
+        }
+
+    }
+}
