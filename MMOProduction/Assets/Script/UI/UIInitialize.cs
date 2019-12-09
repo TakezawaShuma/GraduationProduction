9b8b41a3ce480309dfb11c6925eee791b0a93039
HoriuchiYousuke2020 <horiuchi199720@gmail.com>
2019-12-09 13:43:27 +0900

    フォルダ整理 UI初期フィールド生成prefabの追加

--- MMOProduction/Assets/Script/UI/UIInitialize.cs
+++ MMOProduction/Assets/Script/UI/UIInitialize.cs
@@ -8,18 +8,11 @@ public class UIInitialize : MonoBehaviour
     [SerializeField]
     private int width = 300;
     [SerializeField]
-    private int height = 300; 
-
-    [SerializeField]
-    private bool RightScrollbarFlag;
-    [SerializeField]
-    private bool ButtomScrollbarFlag;
+    private int height = 300;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-
         Vector2 rect = new Vector2(width, height);
         //自分の奴
         this.GetComponent<RectTransform>().sizeDelta = rect;
@@ -33,14 +26,11 @@ public class UIInitialize : MonoBehaviour
         rect.x = rect.x - 16;
         rect.y = rect.y - 66;
 
-        backImage.GetChild(0).GetComponent<RectTransform>().sizeDelta = new Vector2(rect.x, 50.0f);
-        backImage.GetChild(1).GetComponent<RectTransform>().sizeDelta = rect;
+        var titleImage = this.transform.GetChild(1);
+        titleImage.GetComponent<RectTransform>().sizeDelta = new Vector2(rect.x, 50.0f);
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        var fieldImage = this.transform.GetChild(2);
+        fieldImage.GetComponent<RectTransform>().sizeDelta = rect;
 
     }
 }
