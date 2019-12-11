c1c9f02865249f36aa5d88eb3478bcb9ee433c32
nakagawatenma <tenko1177@gmail.com>
2019-12-11 14:22:03 +0900

    選択の少し修正

---
+++ MMOProduction/Assets/Script/CharacterSelectManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterSelectManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 50.0f)){
+                try { hit.collider.gameObject.GetComponent<Animator>().SetBool("pause", true); }
+                catch (ArithmeticException _error) { Debug.LogError(_error); }
+            }
+        }
+    }
+}
