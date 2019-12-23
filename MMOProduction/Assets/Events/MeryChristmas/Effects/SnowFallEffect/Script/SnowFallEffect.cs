b30a59e668645b021ba2e40e3acbef92764cd7c5
s-tama <syuuki0105@gmail.com>
2019-12-23 14:30:35 +0900

    雪をふらせたよん

---
+++ MMOProduction/Assets/Events/MeryChristmas/Effects/SnowFallEffect/Script/SnowFallEffect.cs
@@ -0,0 +1,134 @@
+﻿//
+// SnowFallEffect.cs
+//
+// Author : Tama
+//
+// 降雪を表現するパーティクルエフェクト
+//
+
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class SnowFallEffect : MonoBehaviour
+{
+
+    // シェーダーに渡すバッファ
+    public struct Particle
+    {
+        public Vector3 startPos;
+        public Vector3 pos;
+        public Vector3 vel;
+        public Color col;
+        public float life;
+
+        public Particle(Vector3 startPos, Vector3 pos, Vector3 vel, Color col, float life)
+        {
+            this.startPos = startPos;
+            this.pos = pos;
+            this.vel = vel;
+            this.col = col;
+            this.life = life;
+        }
+    }
+
+    // シェーダープロパティ
+    [System.Serializable]
+    public struct Property
+    {
+        public Texture texture;
+        public float gravity;
+    }
+
+    [SerializeField] Property _property;
+    [SerializeField] Shader _renderingShader = null;       // 描画用シェーダ
+    [SerializeField] ComputeShader _computeShader = null;  // 計算用
+    //[SerializeField] Transform _owner = null;
+
+    private Material _material;
+    private ComputeBuffer _computeBuffer;   // コンピュートシェーダに渡すバッファ
+
+    [SerializeField] int _maxParticles = 1000; // エフェクト最大生成数
+
+    [SerializeField] Transform _target = null;     // 雪を降らせるターゲット
+    [SerializeField] float _range = 250;    // 雪を降らせる範囲
+    [SerializeField] float _height = 250;   // 雪を降らせる高さ
+
+
+    private void Start()
+    {
+        _maxParticles = Mathf.Max(0, _maxParticles);
+
+        _material = new Material(_renderingShader);
+        InitializeComputeBuffer();
+    }
+
+    private void Update()
+    {
+        UpdateComputeBuffer();
+    }
+
+    private void OnRenderObject()
+    {
+        // テクスチャ・バッファをマテリアルに設定
+        _material.SetBuffer("_Particle", _computeBuffer);
+        _material.SetTexture("_MainTex", _property.texture);
+
+        _material.SetPass(0);
+
+        Graphics.DrawProcedural(MeshTopology.Points, _computeBuffer.count);
+    }
+
+    private void OnDestroy()
+    {
+        // コンピュートバッファを開放する
+        SafeRelease(_computeBuffer);
+    }
+
+    // --------------------------------------------
+    // コンピュートバッファの初期化処理
+    // --------------------------------------------
+    private void InitializeComputeBuffer()
+    {
+        // コンピュートバッファを作成する
+        _computeBuffer = new ComputeBuffer(_maxParticles, Marshal.SizeOf(typeof(Particle)));
+
+        Particle[] particle = new Particle[_computeBuffer.count];
+        for (int i = 0; i < _computeBuffer.count; i++)
+        {
+            // データを生成して配列に格納
+            Vector3 startPos = new Vector3(Random.Range(-250f, 250f), Random.Range(0f, 250f), Random.Range(-250f, 250f));
+            //Vector3 startPos = new Vector3(Random.Range(-_range, _range), Random.Range(0, _height), Random.Range(-_range, _range));
+            //startPos += _target.transform.position;
+            particle[i] = new Particle(
+                startPos,
+                startPos,
+                new Vector3(Random.Range(-1f, 1f), -Random.Range(1f, 3f), Random.Range(-1f, 1f)),
+                new Color(1, 1, 1, 1),
+                1
+                ); ;
+        }
+
+        // バッファに適用  
+        _computeBuffer.SetData(particle);
+    }
+
+    // --------------------------------------------
+    // コンピュートバッファの更新処理
+    // --------------------------------------------
+    private void UpdateComputeBuffer()
+    {
+        _computeShader.SetBuffer(0, "_Particle", _computeBuffer);
+        _computeShader.SetFloat("_DeltaTime", Time.deltaTime);
+        _computeShader.SetFloat("_Gravity", _property.gravity);
+        _computeShader.Dispatch(0, _computeBuffer.count / 8 + 1, 1, 1);
+    }
+
+    // --------------------------------------------
+    // コンピュートバッファを安全に開放する
+    // --------------------------------------------
+    private void SafeRelease(ComputeBuffer cb)
+    {
+        if (cb != null)
+            cb.Release();
+    }
+}
---
+++ MMOProduction/Assets/Events/MeryChristmas/Effects/SnowFallEffect/Shader/SnowFallEffect.shader
@@ -0,0 +1,114 @@
+﻿//
+// SnowFallEffect.shader
+//
+// Author : Tama
+//
+
+Shader "Custom/Event/MeryChristmas/SnowFallEffect"
+{
+    SubShader
+    {
+        Tags 
+        { 
+            "RenderType"="Transparent"
+            "Queue"="Transparent"
+        }
+        LOD 100
+
+        Pass
+        {
+            ZWrite On
+            Blend SrcAlpha OneMinusSrcAlpha
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma geometry geom
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct VSIn
+            {
+                uint id : SV_VertexID;
+            };
+
+            struct VSOut
+            {
+                float4 pos : SV_POSITION;
+                float2 uv : TEXCOORD0;
+                float4 col : COLOR;
+            };
+
+            // パーティクルバッファデータ
+            struct Particle
+            {
+                float3 startPos;
+                float3 pos;
+                float3 vel;
+                float4 col;
+                float life;
+            };
+
+            sampler2D _MainTex;
+            StructuredBuffer<Particle> _Particle;   // 構造化バッファ
+
+            VSOut vert(VSIn i)
+            {
+                VSOut o;
+                o.pos = float4(_Particle[i.id].pos, 1);
+                o.uv = float2(0, 0);
+                o.col = _Particle[i.id].col;
+                return o;
+            }
+
+            [maxvertexcount(4)]
+            void geom(point VSOut input[1], inout TriangleStream<VSOut> stream)
+            {
+                VSOut output;
+
+                float4 pos = input[0].pos;
+                float4 col = input[0].col;
+
+                // 四角形になるように頂点を計算
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        // ビルボード行列
+                        float4x4 billboardMatrix = UNITY_MATRIX_V;
+                        billboardMatrix._m03 = 0;
+                        billboardMatrix._m13 = 0;
+                        billboardMatrix._m23 = 0;
+                        billboardMatrix._m33 = 0;
+
+                        // テクスチャ座標
+                        float2 uv = float2(i, j);
+                        output.uv = uv;
+
+                        // 頂点位置を計算
+                        float size = 0.5;
+                        output.pos = pos + mul(float4((uv * 2 - float2(1, 1)) * size, 0, 1), billboardMatrix);
+                        output.pos = mul(UNITY_MATRIX_VP, output.pos);
+
+                        // 色
+                        output.col = col;
+
+                        // ストリームに頂点を追加
+                        stream.Append(output);
+                    }
+                }
+
+                // トライアングルストリップを終了
+                stream.RestartStrip();
+            }
+
+            float4 frag(VSOut i) : SV_Target
+            {
+                float4 col = tex2D(_MainTex, i.uv);
+                col.rgb = 1;
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
