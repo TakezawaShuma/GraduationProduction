0e570ba8be1fa580fc49b16c9389bb30ec34c3c7
HoriuchiYousuke2020 <horiuchi199720@gmail.com>
2020-01-08 12:25:52 +0900

    ダメージ表示の追加

---
+++ MMOProduction/Assets/Script/UI/Damage.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Damage : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas canvas;
+
+    [SerializeField]
+    private GameObject damageParent;
+
+    [SerializeField]
+    private Sprite[] number = new Sprite[10];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InputManager.InputKeyCheckDown(KeyCode.L))
+        {
+            CreateDamageUI(new Vector3(0, 0, 0), 4);
+        }
+    }
+
+    void CreateDamageUI(Vector3 pos, int damage)
+    {
+        var obj = DamageNumber(damage);
+        obj.transform.position = WorldtoCamera(pos);
+
+        obj.transform.parent = canvas.transform;
+    }
+
+    Vector2 WorldtoCamera(Vector3 pos)
+    {
+        Vector2 a = Vector2.zero;
+        return a;
+    }
+
+    GameObject DamageNumber(int num)
+    {
+        var obj = Instantiate(damageParent);
+
+        int n = num;
+        int l = 0;
+        while (n != 0)
+        {
+            int ans = n % 10;
+            n /= 10;
+            Number(ans).transform.parent = damageParent.transform;
+            l++;
+        }
+
+        return obj;
+    }
+
+    //一桁用
+    GameObject Number(int num)
+    {
+        // 画像生成
+        GameObject obj = new GameObject();
+        obj.AddComponent<Image>();
+        obj.GetComponent<Image>().sprite = number[num];
+        return obj;
+    }
+}
---
+++ MMOProduction/Assets/Script/UI/DamageMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageMove : MonoBehaviour
+{
+    [SerializeField]
+    private float alpha = 0;
+
+    [SerializeField]
+    private int time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = this.transform.position;
+
+        if(time < 10)
+        {
+            alpha += 0.1f;
+            pos.y++;
+            
+        }
+        else if(time < 40)
+        {
+           //何もしない 
+        }
+        else if(time < 60)
+        {
+            alpha -= 0.05f;
+            pos.y += 2;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        time++;
+    }
+}
