ca1eacf266c7bf8953a47ac90156f8b31fe4e811
nakagawatenma <tenko1177@gmail.com>
2020-01-10 14:52:22 +0900

    inventory実装

--- MMOProduction/Assets/Script/UI/Inventory.cs
+++ MMOProduction/Assets/Script/UI/Inventory.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Inventory : MonoBehaviour
 {
+    private readonly int MAX_ITEM = 90;
+    private readonly int WIDTH = 9;
+    private readonly int HEIGHT = 10;
     //管理系
     [SerializeField]
-    private List<GameObject> list = null;
+    private List<Image> list = null;
+
+    private List<SlotData> sloatDatas_ = new List<SlotData>();
 
     //生成に必要な物群
     [SerializeField]
@@ -23,37 +28,60 @@ public class Inventory : MonoBehaviour
 
     void Update()
     {
-        //foreach(var a in list)
-        //{
-        //    if(a.GetComponent<SlotData>().GetUseFlag())
-        //    {
-        //        //アイテムを使う
-        //        var b = a.GetComponent<SlotData>().HOGE;
-        //        var c = a.GetComponent<SlotData>().ID;
-
-
-        //        //使った報告
-        //        a.GetComponent<SlotData>().UseFlagOff();
-        //    }
-        //}
     }
 
     private void GenerateInventory()
     {
         Vector3 pos = new Vector3((-240 + 8 + 8), 265 - 8 - 50 - 8 - 24 - 24, 0);
 
-        for (int i = 0; i < 9; i++)
-        {
-            for (int j = 0; j < 10; j++)
-            {
-                GameObject obj = Instantiate(Slot);
-                obj.transform.parent = this.transform;
-                obj.transform.localPosition = pos + new Vector3(j * 48, i * -48, 0);
-                obj.transform.localScale = new Vector3(1, 1, 1);
-                obj.transform.GetChild(0).GetComponent<Image>().sprite = sprite;
+        int x = 0;int y = 0;
+        for(int i = 0; i < MAX_ITEM; i++) {
+            x = i % HEIGHT;
+            y = i / HEIGHT;
 
-                list.Add(obj);
-            }
+            GameObject obj = Instantiate(Slot, transform);
+            obj.transform.localPosition = pos + new Vector3(x * 48, y * -48, 0);
+            obj.transform.localScale = new Vector3(1, 1, 1);
+            list.Add(obj.transform.GetChild(0).GetComponent<Image>());
+        }
+        ImageCheck();
+    }
+
+    // TODO: 画像のマスター作成後交換
+    public bool AddItem(int _id) {
+        if (sloatDatas_.Count >= MAX_ITEM) return false;
+        if (_id <= 0) return false;
+
+        SlotData data = new SlotData();
+        data.ID = _id;
+        sloatDatas_.Add(data);
+        return true;
+    }
+
+    public void ChangeItems(int[] _ids) {
+        List<Packes.AccessoryMasterData> imageName = AccessoryDatas.Find(_ids);
+        foreach(var id in _ids) {
+            if (id <= 0) continue;
+
+            SlotData data = new SlotData();
+            data.ID = id;
+            sloatDatas_.Add(data);
+        }
+    }
+
+    public void ChangeItems(List<int> _ids) {
+        List<Packes.AccessoryMasterData> imageName = AccessoryDatas.Find(_ids);
+        foreach(var id in _ids) {
+            if (id <= 0) continue;
+            SlotData data = new SlotData();
+            data.ID = id;
+            sloatDatas_.Add(data);
+        }
+    }
+
+    private void ImageCheck() { 
+        for(int i = 0; i < sloatDatas_.Count; i++) {
+            list[i].sprite = sprite;
         }
     }
 }
