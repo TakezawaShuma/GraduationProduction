7bae9f8dc2c184a4d37b0a36d0ef798594fe134f
SugiuraMakoto <margin9876@gmail.com>
2020-02-03 11:06:23 +0900

    修正

--- MMOProduction/Assets/Script/MapMovePoint.cs
+++ MMOProduction/Assets/Script/MapMovePoint.cs
@@ -38,7 +38,6 @@ public class MapMovePoint : MonoBehaviour
             manager = GameObject.Find("PlaySceneManager").GetComponent<PlaySceneManager>();
 
             manager.SendMoveMap(UserRecord.NextMapId);
-            currentTime = 0;
         }
     }
 
